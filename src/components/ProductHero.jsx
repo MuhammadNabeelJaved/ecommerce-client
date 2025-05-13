5d914ce02a527cfd8da138c1b997492d11d01333
Nabeel <muhammadnabeeljavede@gmail.com>
2025-05-13 15:54:48 +0500

    Add BOOMBOX image and update ProductHero component layout and styling

--- src/components/ProductHero.jsx
+++ src/components/ProductHero.jsx
@@ -1,16 +1,28 @@
 import React from "react";
+import boomBox from "../assets/BOOMBOX.png";
 
 const ProductHero = () => {
   return (
     <>
-      <div className="w-full max-w-full mx-auto px-4 sm:px-6 md:px-8 lg:px-[135px]">
-        <div className="flex items-center flex-wrap text-white bg-black justify-between gap-5 md:gap-28 w-full">
-          <div></div>
-          <div>
-            <img
-              src="https://images.unsplash.com/photo-1523278334708-0e3c02c02d6d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"
-              alt=""
-            />
+      <div className="w-full max-w-full mx-auto mb-8 px-4 sm:px-6 md:px-8 lg:px-[135px]">
+        <div className="flex items-center font-poppins px-14 py-16 flex-wrap text-white bg-black justify-center gap-5 md:gap-28 w-full">
+          <div className="flex flex-col items-start gap-8 w-1/2">
+            <p className="text-[#0F6]">Categories</p>
+            <h2 className="text-5xl text-white">
+              Enhance Your Music Experience
+            </h2>
+
+            <div className="rounded-full w-16 h-16 flex-col bg-white text-black flex items-center justify-center">
+              <p className="text-black font-poppins font-[600]">23</p>
+              <p className="text-[11px] font-[400]">Hours</p>
+            </div>
+
+            <button className="bg-[#0F6] font-poppins text-center hover:bg-[rgba(89,255,139,0.98)] transition-all delay-75 cursor-pointer rounded-[4px] text-white py-4 px-12">
+              Buy Now!
+            </button>
+          </div>
+          <div className="">
+            <img className="w-lg " src={boomBox} alt="" />
           </div>
         </div>
       </div>
