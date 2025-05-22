1b61d1a9aa0ebb31667134813532b041ab891960
Nabeel <muhammadnabeeljavede@gmail.com>
2025-05-23 02:17:05 +0500

    Add Contact component with form and breadcrumb for improved user interaction

---
+++ src/components/Contact.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const ContactFrom = () => {
+  return (
+    <>
+      <div className="flex justify-between gap-8 items-center">
+        <div className="flex justify-center items-center flex-col p-10 shadow-neutral-400 shadow-md rounded-lg">
+          <div className="flex flex-col gap-8">
+            <div>
+              <div>
+                <h1 className="text-center font-poppins">Contact Us</h1>
+              </div>
+            </div>
+            <div></div>
+          </div>
+        </div>
+        <div></div>
+      </div>
+    </>
+  );
+};
+
+export default ContactFrom;
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,21 +1,12 @@
 import React from "react";
 import Breadcrumb from "../components/Breadcrumb";
+import ContactFrom from "../components/Contact.jsx";
 
 const Contact = () => {
   return (
     <div className="w-full max-w-full mx-auto px-4 sm:px-6 md:px-8 lg:px-[135px]">
-      <Breadcrumb
-        items={[
-          { id: "home", label: "Home" },
-          { id: "products", label: "Products" },
-          { id: "category", label: "Electronics" },
-        ]}
-        onNavigate={(item) => {
-          // Handle navigation logic
-          console.log("Navigate to:", item);
-        }}
-      />
-      Contact
+      <Breadcrumb />
+      <ContactFrom />
     </div>
   );
 };
