b7ecba4fbc9b5e9ea8cea116483ecc89514c1c69
Nabeel <muhammadnabeeljavede@gmail.com>
2025-05-24 12:16:24 +0500

    Add Status component and integrate it into About page for improved status display

---
+++ src/components/Status.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const Status = () => {
+  const statusData = [
+    {
+      id: 1,
+      icon: "",
+      title: "Pending",
+      subTitle: 2,
+    },
+  ];
+  return (
+    <>
+      <div>Status</div>
+    </>
+  );
+};
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import Breadcrumb from "../components/Breadcrumb";
 import AboutHero from "../components/AboutHero";
+import { Status } from "../components/Status";
+import Features from "../components/Features";
 
 const About = () => {
   return (
     <>
       <div className="w-full max-w-full mx-auto px-4 sm:px-6 md:px-8 lg:px-[135px]">
         <Breadcrumb />
-        <AboutHero/>
+        <AboutHero />
+        <Status />
+        <Features />
       </div>
     </>
   );
