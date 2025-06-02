15effcebdec3fb97014d01cedb461bbcc5e46f32
Nabeel <muhammadnabeeljavede@gmail.com>
2025-06-03 00:50:21 +0500

    Refactor ProductView component layout for improved structure and styling

--- src/components/ProductView.jsx
+++ src/components/ProductView.jsx
@@ -21,7 +21,43 @@ const ProductView = () => {
   console.log("Product ID:", id);
   return (
     <>
-      <div className="">ProductView</div>
+      <div className="flex justify-between gap-14 items-center">
+        <div className="flex max-h-auto justify-center items-center gap-8">
+          <div className="flex flex-col gap-4">
+            <div className="bg-[#F5F5F5] items-center flex flex-col gap-4 justify-center p-6 rounded-[4px]">
+              <img className="w-[120px]" src={filterdProduct.image} alt="" />
+            </div>
+            <div className="bg-[#F5F5F5] items-center flex flex-col justify-center p-6 rounded-[4px]">
+              <img className="w-[120px]" src={filterdProduct.image} alt="" />
+            </div>
+            <div className="bg-[#F5F5F5] items-center flex flex-col justify-center p-6 rounded-[4px]">
+              <img className="w-[120px]" src={filterdProduct.image} alt="" />
+            </div>
+            <div className="bg-[#F5F5F5] items-center flex flex-col justify-center p-6 rounded-[4px]">
+              <img className="w-[120px]" src={filterdProduct.image} alt="" />
+            </div>
+            <div className="bg-[#F5F5F5] items-center flex flex-col justify-center p-6 rounded-[4px]">
+              <img className="w-[120px]" src={filterdProduct.image} alt="" />
+            </div>
+          </div>
+
+          <div className="flex bg-[#F5F5F5] px-[27px] py-[204px] justify-center items-center w-[500px] rounded-[4px]">
+            <img className="w-[450px]" src={filterdProduct.image} alt="" />
+          </div>
+        </div>
+        <div>
+          <div>
+            <h1 className="font-poppins text-black text-2xl font-[600]">
+              {filterdProduct.title.replace(/-/g, " ")}
+            </h1>
+            <p className="text-[#FF0000] text-[20px] font-bold">
+              ${filterdProduct.price}
+            </p>
+            <p>{filterdProduct.description}</p>
+          </div>
+          <div></div>
+        </div>
+      </div>
     </>
   );
 };
