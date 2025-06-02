962d6aa7797ab73b566d533520bfa39be19844fd
Nabeel <muhammadnabeeljavede@gmail.com>
2025-06-02 22:19:03 +0500

    Add AccountComp component and integrate it into Account page

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Breadcrumb from "../components/Breadcrumb";
+import AccountComp from "../components/AccountComp";
 
 const Account = () => {
   const [user, setUser] = useState("Alina");
@@ -8,6 +9,7 @@ const Account = () => {
     <>
       <div className="w-full max-w-full mx-auto px-4 sm:px-6 md:px-8 lg:px-[135px]">
         <Breadcrumb user={user} />
+        <AccountComp />
       </div>
     </>
   );
