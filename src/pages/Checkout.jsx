8e72b60bd6ba358e71e4104953e79659bf38eeff
Nabeel <muhammadnabeeljavede@gmail.com>
2025-06-07 23:55:57 +0500

    Add Breadcrumb component to Checkout page for improved navigation

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import BillingDetails from "../components/Billing";
+import Breadcrumb from "../components/Breadcrumb";
 
 const Checkout = () => {
   return (
     <>
       <div className="w-full max-w-full mx-auto px-4 sm:px-6 md:px-8 lg:px-[135px]">
+        <Breadcrumb />
         <BillingDetails/>
       </div>
     </>
