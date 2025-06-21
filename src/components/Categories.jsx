9a1ba4bdb803ad0025fddbbdadfd5508d60a7841
Muhammad Nabeel Javed <muhammadnabeeljavede@gmail.com>
2025-06-22 01:06:28 +0500

    Wrap category items in NavLink for improved navigation to product pages

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -6,6 +6,7 @@ import gaming from "../assets/Category-Gamepad.png";
 import camera from "../assets/Category-Camera.png";
 import headphones from "../assets/Category-Headphone.png";
 import SmartWatch from "../assets/Category-SmartWatch.png";
+import { NavLink } from "react-router";
 
 const Categories = () => {
   const [selectedCategory, setSelectedCategory] = useState(false);
@@ -34,7 +35,8 @@ const Categories = () => {
         <div className="py-20">
           <div className="flex justify-center items-center flex-wrap gap-8">
             {categories.map((category, index) => (
-              <div
+              <NavLink
+                to={`/products/${category.title.toLowerCase()}`}
                 onClick={() => selectCategory(category.title)}
                 key={index}
                 className={`flex justify-center max-w-[170px] max-h-[145px] items-center px-14 py-6 border-[1px] border-[rgba(0, 0, 0, 0.30] rounded-[4px] cursor-pointer transition duration-300 ease-in-out ${
@@ -47,7 +49,7 @@ const Categories = () => {
                   <img className="w-[56px]" src={category.icon} alt="" />
                   <h2 className="text-lg font-semibold">{category.title}</h2>
                 </div>
-              </div>
+              </NavLink>
             ))}
           </div>
         </div>
